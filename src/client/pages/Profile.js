0e89ba6c2cd8b8fa5c01d1e90ff77129e98203bc
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-03-24 17:44:22 -0400

    Implement basic functionality for admin button

--- src/client/pages/Profile.js
+++ src/client/pages/Profile.js
@@ -35,12 +35,23 @@ const Profile = () => {
     }
   }, []);
 
+  let adminBtnBase =
+    'ml-3 inline-flex justify-end py-2 px-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2';
+  let adminBtnFormat =
+    userDoc && !userDoc.isAdmin
+      ? ' bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500'
+      : ' bg-red-600 hover:bg-red-700 focus:ring-red-500';
+  const adminBtnClasses = adminBtnBase + adminBtnFormat;
+
   return (
     <main>
       <Card>
         <h1 className="text-2xl leading-6 font-medium text-gray-900">
-          {userDoc?.uid === user?.uid ? "Edit your profile" :
-          adminMode ? 'Edit user profile' : "View profile"}
+          {userDoc?.uid === user?.uid
+            ? 'Edit your profile'
+            : adminMode
+            ? 'Edit user profile'
+            : 'View profile'}
         </h1>
       </Card>
 
@@ -57,6 +68,31 @@ const Profile = () => {
           </>
         )}
       </LoadingError>
+      <Card>
+        <h3 className="text-lg leading-5 font-medium text-gray-900">
+          Admin Status
+        </h3>
+        <p className="mt-1 text-sm text-gray-500">
+          {userDoc && !userDoc.isAdmin ? 'No admin access' : 'Admin access'}
+        </p>
+        {!adminMode && (
+          <p className="mt-1 text-xs text-gray-400">
+            {' '}
+            You are not authorized to grant or revoke admin access{' '}
+          </p>
+        )}
+        <div className="pt-2 flex justify-end">
+          <button
+            type="button"
+            className={adminBtnClasses}
+            onClick={() =>
+              updateUser(userDoc.uid, { isAdmin: !userDoc.isAdmin })
+            }
+          >
+            {userDoc && !userDoc.isAdmin ? 'Grant' : 'Revoke'}
+          </button>
+        </div>
+      </Card>
     </main>
   );
 };
